342a2c99d50960bd0dc608f6dfa219378ca52bf1
Uday Mehta <iamudaymehta@gmail.com>
2025-02-25 21:46:45 +0530

    PF-02 : Update basic setup (#2)
    * Fix: Typo in App Title
    
    * chore: Add global style for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
-  return <h1>Hello, React!</h1>;
+  return (
+    <Home />
+  );
 };
 
 export default App;
